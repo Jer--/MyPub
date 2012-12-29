b7445babc7e2eff1e112e19d885bd771f7e9ba20
Jérémie <jeremie.coquin@gmail.com>
2012-12-30 00:13:11 +0100

    Adding header comments (licence)

--- grails-app/domain/mypub/Coment.groovy
+++ grails-app/domain/mypub/Coment.groovy
@@ -1,24 +1,23 @@
 /*******************************************************************************
- * Author : Group BBHC
- * Licence : AGPL v3
+*  Author : Group BBHC
+*  Licence : AGPL v3
  ******************************************************************************/
 package mypub
 
 class Coment {
-	
+
 	String username
 	Date postDate
 	String text
-	
+
 	static belongsTo = [pub : Pub]
 
-	String toString () {return 'Comment by : ' + username + 
-								' date :' + postDate +
-								 ' / ' + text
-								 }
-	
-	
-    static constraints = {
-		text size: 1..160
-    }
+	String toString () {
+		return 'Comment by : ' + username +
+		' date :' + postDate +
+		' / ' + text
+	}
+
+
+	static constraints = { text size: 1..160 }
 }
