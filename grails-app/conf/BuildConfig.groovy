2ab8b5e55b11569ea58f1562a60b63c14f9d0864
Jérémie <jeremie.coquin@gmail.com>
2012-12-31 19:05:09 +0100

    Remove unused comments Rename the war file

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -8,7 +8,7 @@ grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 grails.project.target.level = 1.6
 grails.project.source.level = 1.6
-//grails.project.war.file = "target/${appName}-${appVersion}.war"
+grails.project.war.file = "target/${appName}.war"
 
 grails.project.dependency.resolution = {
 	// inherit Grails' default dependencies
