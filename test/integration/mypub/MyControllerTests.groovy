1c8e450ce146f9a1de425f493cef0441da601816
Brion Benjamin <bjm.brion@gmail.com>
2013-01-05 11:39:36 +0100

    Integration tests. First step

---
+++ test/integration/mypub/MyControllerTests.groovy
@@ -0,0 +1,37 @@
+package mypub
+
+import static org.junit.Assert.*
+import grails.test.GrailsUnitTestCase
+
+import org.junit.*
+
+/**
+ * Scenario integration tests:
+ * This scenario allows us to test the different possible use cases do by users
+ * of the web application MyPub.
+ * Steps:
+ * - Add three users (A, B and C).
+ * - Delete User C.
+ * - Creating a friendship bond between users A and B.
+ * - Add a bar with user A edit.
+ * - Add a comment with user B.
+ * - Delete comment with user B.
+ * - Change the description of the bar with user B.
+ * - User B removes his friends the user A.
+ */
+class MyControllerTests extends GrailsUnitTestCase {
+
+	@Before
+	protected void setUp() {
+	}
+
+	@After
+	void tearDown() {
+		// Tear down logic here
+	}
+
+	@Test
+	void testSomething() {
+		fail "Implement me"
+	}
+}
