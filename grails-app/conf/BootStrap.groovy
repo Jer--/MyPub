50eb83b714933786c3b5de4bb9ca2539fa81896c
Brion Benjamin <bjm.brion@gmail.com>
2013-01-05 21:25:24 +0100

    Adds Unit tests. Modification of bootstrap...

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -12,8 +12,14 @@ class BootStrap {
 		
 		if(!User.count()) {
 			new User(username:'admin',  enabled: true, password:'admin', firstName:'Admin', lastName:'Admin', mail:'johndu12@hotmail.fr').save(failOnError:true)
+			new User(username:'ben',  enabled: true, password:'admin', firstName:'Brion', lastName:'Benjamin', mail:'brion.benjamin@gmail.com').save(failOnError:true)
+			new User(username:'jer',  enabled: true, password:'admin', firstName:'Coquin', lastName:'Jeremie', mail:'coquin.jeremie@gmail.com').save(failOnError:true)
+			new User(username:'val',  enabled: true, password:'admin', firstName:'Heuillet', lastName:'Valerie', mail:'valerie.heuillet@gmail.com').save(failOnError:true)
 			}
 		UserRole.create User.findByUsername('admin'), adminRole, true
+		UserRole.create User.findByUsername('ben'), adminRole, true
+		UserRole.create User.findByUsername('jer'), adminRole, true
+		UserRole.create User.findByUsername('val'), adminRole, true
 		
     }
     def destroy = {
---
+++ test/unit/mypub/UserTests.groovy
@@ -0,0 +1,120 @@
+/*******************************************************************************
+*  Author : Group BBHC
+*  Licence : AGPL v3
+ ******************************************************************************/
+package mypub
+
+
+
+import grails.test.mixin.*
+
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(User)
+class UserTests {
+	
+	void testconstraint() {
+		 def existingUser = new User(username:"userTest", password:"passwordTest", firstName:"Delon", lastName:"Alain", sexe:'M', mail:"testmail@test.com")
+		 
+		 mockForConstraintsTests(User, [existingUser])
+		 
+		 /* 
+		  * Validation success.
+		  */
+		 def user = new User(username:"userTest1", password:"passwordTest", firstName:"Ferrat", lastName:"Luc", mail:"testmail@test.com")
+		 assert user.validate()
+		 
+		 /*
+		  * Validation not success.
+		  */
+		 
+		 /*
+		  * The following attributes can be NULLABLE:
+		  * - DDN
+		  * - sex
+		  * - friends
+		  * - pictures
+		  * - pubs
+		  * Others attributes shall be indicated when User creation.
+		  */
+		 
+		 // UserName is not NULLABLE.
+		 user = new User(password:"passwordTest", firstName:"Patrick", lastName:"Sebastien", sexe:'M', mail:"testmail@test.com")
+		 assert !user.validate()
+		 assert "nullable" == user.errors["username"]
+		 
+		 // Password is not NULLABLE.
+		 user = new User(username:"userTest2", firstName:"Rizoli", lastName:"Phillipe", sexe:'M', mail:"testmail@test.com")
+		 assert !user.validate()
+		 assert "nullable" == user.errors["password"]
+		 
+		 // FirstName is not NULLABLE.
+		 user = new User(username:"userTest3", password:"passwordTest", lastName:"Mati", sexe:'M', mail:"testmail@test.com")
+		 assert !user.validate()
+		 assert "nullable" == user.errors["firstName"]
+		 
+		 // LastName is not NULLABLE.
+		 user = new User(username:"userTest4", password:"passwordTest", firstName:"Sabatier", sexe:'M', mail:"testmail@test.com")
+		 assert !user.validate()
+		 assert "nullable" == user.errors["lastName"]
+		 
+		 // Mail is not NULLABLE.
+		 user = new User(username:"userTest5", password:"passwordTest", firstName:"Vilard", lastName:"Herve")
+		 assert !user.validate()
+		 assert "nullable" == user.errors["mail"]
+		 
+		 /*
+		  * The following attributes can not be blank:
+		  * - userName
+		  * - password
+		  * - firstName
+		  * - lastName
+		  * - mail
+		  * - DDN
+		  */
+		 
+		 // UserName not be blank.
+		 user = new User(username:"", password:"passwordTest", firstName:"Blanc", lastName:"Michel", mail:"testmail@test.com")
+		 assert !user.validate()
+		 assert "blank" == user.errors["username"]
+		 
+		 // Password not be blank.
+		 user = new User(username:"userTest6", password:"", firstName:"Torr", lastName:"Michelle", mail:"testmail@test.com")
+		 assert !user.validate()
+		 assert "blank" == user.errors["password"]
+		 
+		 // FirstName not be blank.
+		 user = new User(username:"userTest7", password:"passwordTest", firstName:"", lastName:"Gregory", mail:"testmail@test.com")
+		 assert !user.validate()
+		 assert "blank" == user.errors["firstName"]
+		 
+		 // LastName not be blank.
+		 user = new User(username:"userTest8", password:"passwordTest", firstName:"Bardou", lastName:"", mail:"testmail@test.com")
+		 assert !user.validate()
+		 assert "blank" == user.errors["lastName"]
+		 
+		 // FirstName not be blank.
+		 user = new User(username:"userTest9", password:"passwordTest", firstName:"Thuram", lastName:"Lilian", mail:"")
+		 assert !user.validate()
+		 assert "blank" == user.errors["mail"]
+		 
+		 // Mail must be written as email.
+		 user = new User(username:"userTest10", password:"passwordTest", firstName:"Leta", lastName:"Nico", mail:"notemailatpointcom")
+		 assert !user.validate()
+		 assert "email" == user.errors["mail"]
+		 
+		 // Sex shall be in List ['M', 'F'].
+		 user = new User(username:"userTest", password:"passwordTest", firstName:"Zemmour", lastName:"Eric", sexe:'U', mail:"notemailatpointcom")
+		 assert !user.validate()
+		 assert "inList" == user.errors["sexe"]
+		 
+		 // Max DDN shall be new Date().minus(3650).
+		 Date dateTest = new Date().minus(2650)
+		 user = new User(username:"userTest10", password:"passwordTest", firstName:"Bayrou", lastName:"Francois", mail:"notemailatpointcom", ddn: dateTest)
+		 assert !user.validate()
+		 assert "max" == user.errors["ddn"]
+	}
+}
