46de7f47475a6b6c870a80ab44cedb9ac91071c9
Brion Benjamin <bjm.brion@gmail.com>
2013-01-13 20:21:56 +0100

    Add integration tests #27

--- test/integration/mypub/ScenarioPictureTests.groovy
+++ test/integration/mypub/ScenarioPictureTests.groovy
@@ -26,49 +26,93 @@ import org.junit.*
 class ScenarioPictureTests extends GroovyTestCase {
 
 	def sessionFactory
-
-
+	Pub pub
+	User userA, userB, userC
 
 	@Before
 	void setUp() {
-		/*
-		 * Enhancement of integration test:
-		 *
-		 * sessionFactory.currentSession.flush()
-		 * sessionFactory.currentSession.clear()
-		 * drop database <myTestDatabase>
-		 * create database <myTestDatabase>
-		 */
-		log.info("[TestAppTests] Start integration test for MyPub App.")
-		log.info("[TestAppTests] Launch method: pubContextInit.")
-		// pubContextInit()
-		log.info("[TestAppTests] Launch method: userContextInit.")
-		// userContextInit()
-		log.info("[TestAppTests] Set up success.")
-	}
+		@Before
+		void setUp() {
+			log.info("[ScenarioPictureTests] setUp(): Start integration test for MyPub App.")
 
-	@Test
-	void testSomething() {
-		log.info("[TestAppTests] Launch method: deleteUserC().")
-		// deleteUserC()
-		log.info("[TestAppTests] Launch method: friendShipAB().")
-		// friendShipAB()
-		log.info("[TestAppTests] Launch method: createPubByA().")
-		// createPubByA()
-		log.info("[TestAppTests] Launch method: addCommentWithB().")
-		// addCommentWithB()
-		log.info("[TestAppTests] Launch method: deleteCommentWithB().")
-		// deleteCommentWithB()
-		log.info("[TestAppTests] Launch method: changeDescriptionPubWithB().")
-		// changeDescriptionPubWithB()
-		log.info("[TestAppTests] Launch method: deleteFriendBA().")
-		// deleteFriendBA()
-	}
+			log.info("[ScenarioPictureTests] setUp(): Flush the session (delete a pub and 3 Users if existed.")
 
-	@After
-	void tearDown() {
-		// userContextClose()
-		// pubContextInit()
-		log.info("[TestAppTests] Finish!.")
+			println "[setUp()] Nombre de pubs avant suppression du pub test : " + Pub.count()
+			Pub.findByName("Pub des tests")?.delete()
+			println "[setUp()] Nombre de pubs apres suppession du pub test : " + Pub.count()
+
+			println "[setUp()] Nombre d'utilisateurs avant suppression des users tests : " + User.count()
+			User.findByUsername("userA")?.delete()
+			User.findByUsername("userB")?.delete()
+			User.findByUsername("userC")?.delete()
+			println "[setUp()] Nombre d'utilisateurs apres suppression des users tests : " + User.count()
+
+			int nbUsers = User.count()
+			int nbPubs = Pub.count()
+			print "[setUp()] Nombre d'utilisateurs avant ajout des users tests : " + nbUsers
+			println ", et de pubs : " + nbPubs
+			log.info("[ScenarioPictureTests] setUp(): Add a pub and 3 users.")
+			pub = new Pub(name: "Pub des tests", address: "address", city: "Toulouse", zip: "97400", type: "PUB").save(failOnError:true)
+			userA = new User(username:"userA",  enabled: true, password:"passwordA", firstName:"Hobitt", lastName:"bilbo", mail:"bjm.brion@gmail.com").save(failOnError:true)
+			userB = new User(username:"userB",  enabled: true, password:"passwordB", firstName:"johanson", lastName:"scarlett", mail:"beauty@gmail.com").save(failOnError:true)
+			userC = new User(username:"userC",  enabled: true, password:"passwordC", firstName:"filipo", lastName:"goz", mail:"filipo@gmail.com").save(failOnError:true)
+			pub.setBelongsTo(userA)
+			print "[setUp()] Nombre d'utilisateurs apres ajout des users tests : " + User.count()
+			println ", et de pubs : " + Pub.count()
+
+			assert (nbUsers+3) == User.count()
+			assert (nbPubs+1) == Pub.count()
+
+			log.info("[ScenarioPictureTests] Set up success.")
+
+			/*
+			 * Enhancement of integration test:
+			 *
+			 * sessionFactory.currentSession.flush()
+			 * sessionFactory.currentSession.clear()
+			 * drop database <myTestDatabase>
+			 * create database <myTestDatabase>
+			 */
+			log.info("[ScenarioPictureTests] Set up end.")
+		}
+
+		@Test
+		void testSomething() {
+			log.info("[ScenarioPictureTests] Launch method: deleteUserC().")
+		}
+
+		@After
+		void tearDown() {
+			log.info("[ScenarioPictureTests] Tear Down begin.")
+
+			log.info("[ScenarioPictureTests] Tear Down: toString tests.")
+
+			println "[tearDown()] Info pub test : " + Pub.findByName("pub").toString()
+			println "[tearDown()] Info UserA test : " + User.findByUsername("userA").toString()
+			println "[tearDown()] Info UserB test : " + User.findByUsername("userB").toString()
+			println "[tearDown()] Info UserC test: " + User.findByUsername("userC").toString()
+
+			print "[tearDown()] Nombre d'utilisateurs avant suppression des users tests : " + User.count()
+			println ", et de pubs : " + Pub.count()
+
+			int nbUsers = User.count()
+			int nbPubs = Pub.count()
+
+			log.info("[ScenarioPictureTests] Tear Down: delete 3 users tests.")
+			userA?.delete()
+			userB?.delete()
+			userC?.delete()
+
+			log.info("[ScenarioPictureTests] Tear Down: delete a pub test.")
+			pub.delete()
+
+			print "[tearDown()] Nombre d'utilisateurs apres suppression des users tests : " + User.count()
+			println ", et de pubs : " + Pub.count()
+
+			assert (nbUsers-3) == User.count()
+			assert (nbPubs-1) == Pub.count()
+
+			log.info("[ScenarioPictureTests] Tear down end.")
+			log.info("[ScenarioPictureTests] Finish!.")
+		}
 	}
-}
--- test/integration/mypub/ScenarioPubTests.groovy
+++ test/integration/mypub/ScenarioPubTests.groovy
@@ -14,9 +14,42 @@ import org.junit.*
  */
 class ScenarioPubTests extends GroovyTestCase {
 
+	Pub pub
+	User userA, userB, userC
 	@Before
 	void setUp() {
 		log.info("[ScenarioPubTests] setUp(): Start integration test for MyPub App.")
+
+		log.info("[ScenarioPubTests] setUp(): Flush the session (delete a pub and 3 Users if existed.")
+
+		println "[setUp()] Nombre de pubs avant suppression du pub test : " + Pub.count()
+		Pub.findByName("Pub des tests")?.delete()
+		println "[setUp()] Nombre de pubs apres suppession du pub test : " + Pub.count()
+
+		println "[setUp()] Nombre d'utilisateurs avant suppression des users tests : " + User.count()
+		User.findByUsername("userA")?.delete()
+		User.findByUsername("userB")?.delete()
+		User.findByUsername("userC")?.delete()
+		println "[setUp()] Nombre d'utilisateurs apres suppression des users tests : " + User.count()
+
+		int nbUsers = User.count()
+		int nbPubs = Pub.count()
+		print "[setUp()] Nombre d'utilisateurs avant ajout des users tests : " + nbUsers
+		println ", et de pubs : " + nbPubs
+
+		log.info("[ScenarioPubTests] setUp(): Add a pub and 3 users.")
+		pub = new Pub(name: "Pub des tests", address: "address", city: "Toulouse", zip: "97400", type: "PUB").save(failOnError:true)
+		userA = new User(username:"userA",  enabled: true, password:"passwordA", firstName:"Hobitt", lastName:"bilbo", mail:"bjm.brion@gmail.com").save(failOnError:true)
+		userB = new User(username:"userB",  enabled: true, password:"passwordB", firstName:"johanson", lastName:"scarlett", mail:"beauty@gmail.com").save(failOnError:true)
+		userC = new User(username:"userC",  enabled: true, password:"passwordC", firstName:"filipo", lastName:"goz", mail:"filipo@gmail.com").save(failOnError:true)
+		pub.setBelongsTo(userA)
+		print "[setUp()] Nombre d'utilisateurs apres ajout des users tests : " + User.count()
+		println ", et de pubs : " + Pub.count()
+
+		assert (nbUsers+3) == User.count()
+		assert (nbPubs+1) == Pub.count()
+
+		log.info("[ScenarioPubTests] Set up success.")
 	}
 
 	@Test
@@ -25,6 +58,36 @@ class ScenarioPubTests extends GroovyTestCase {
 
 	@After
 	void tearDown() {
+		log.info("[ScenarioPubTests] Tear Down begin.")
+
+		log.info("[ScenarioPubTests] Tear Down: toString tests.")
+
+		println "[tearDown()] Info pub test : " + Pub.findByName("pub").toString()
+		println "[tearDown()] Info UserA test : " + User.findByUsername("userA").toString()
+		println "[tearDown()] Info UserB test : " + User.findByUsername("userB").toString()
+		println "[tearDown()] Info UserC test: " + User.findByUsername("userC").toString()
+
+		print "[tearDown()] Nombre d'utilisateurs avant suppression des users tests : " + User.count()
+		println ", et de pubs : " + Pub.count()
+
+		int nbUsers = User.count()
+		int nbPubs = Pub.count()
+
+		log.info("[ScenarioPubTests] Tear Down: delete 3 users tests.")
+		userA?.delete()
+		userB?.delete()
+		userC?.delete()
+
+		log.info("[ScenarioPubTests] Tear Down: delete a pub test.")
+		pub.delete()
+
+		print "[tearDown()] Nombre d'utilisateurs apres suppression des users tests : " + User.count()
+		println ", et de pubs : " + Pub.count()
+
+		assert (nbUsers-3) == User.count()
+		assert (nbPubs-1) == Pub.count()
+
+		log.info("[ScenarioPubTests] Tear down end.")
 	}
 
 	private Object save(Object object ) {
