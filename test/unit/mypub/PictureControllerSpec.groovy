1e4ed5dab0a6e3dcf55651bc96edf5cd36f611b3
Valérie <valerie.heuillet@gmail.com>
2013-02-10 21:28:28 +0100

    UT spock with springsecurityService #54

--- test/unit/mypub/PictureControllerSpec.groovy
+++ test/unit/mypub/PictureControllerSpec.groovy
@@ -3,9 +3,18 @@ package mypub
 import grails.plugin.spock.UnitSpec
 import grails.plugins.Spock.lang.*
 import grails.test.mixin.*
+import grails.plugins.springsecurity.SpringSecurityService
 
 @TestFor(PictureController)
+@Mock(Picture)
 class PictureControllerSpec extends UnitSpec {
+	def setUpSpringSecurity(User u) {
+		
+				def security = mockFor(SpringSecurityService)
+				security.metaClass.getCurrentUser = { -> return u }
+				return security
+	}
+	
 	def 'Index action should redirect to list page'() {
 		when:
 		controller.index()
@@ -33,5 +42,31 @@ class PictureControllerSpec extends UnitSpec {
 		then:
 		assert model.pictureInstanceList.size() == 1
 		assert model.pictureInstanceTotal == 1
+	}
+	
+	def 'action save should add the picture to the current user pictures list'() {
+		setup:
+		mockDomain(User)
+		
+		def userInstance = new User(username: 'user1',
+									password: 'pass1',
+									firstName: 'alfred',
+									lastName: 'alfredaussi',
+									mail: 'alfred@john.fr')
+	   assert userInstance.validate()
+	   assert userInstance.save() != null
+	
+	   //SpringSecurityService.metaClass.getCurrentUser = { -> return userInstance }
+	   controller.springSecurityService =setUpSpringSecurity(userInstance)
+	   
+	   when:
+	   params["name"] = 'pictest'
+	   params["data"]= [1,1,1,1]
+	   controller.save()
+	   
+	   then:
+	   assert response.redirectedUrl == '/picture/showPerso/1'
+	   assert controller.flash.message != null
+	   assert Picture.count() == 1
 	}
 }
